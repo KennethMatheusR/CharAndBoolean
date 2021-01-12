00bc5d89a19bc3305f4efe740d7ee400c667f917
KENNETH MATHEUS R <kenneth.matheus@gmail.com>
2021-01-12 20:00:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+
+    char myChar = 'D';
+    char myUnicode = '\u0044';
+    System.out.println(myChar);
+    System.out.println(myUnicode);
+
+  }
+}
