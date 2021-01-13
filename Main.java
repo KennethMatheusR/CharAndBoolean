19050ea798a47b8b0aee83acfb9502ac8b7889fc
KENNETH MATHEUS R <kenneth.matheus@gmail.com>
2021-01-13 14:22:34 +0000

    got reminder challenge boring

--- Main.java
+++ Main.java
@@ -6,5 +6,33 @@ class Main {
     System.out.println(myChar);
     System.out.println(myUnicode);
 
+  boolean myTrueBooleanValue = true;
+  boolean myFalseBooleanvalue = false;
+
+  boolean iscustomerOverTwentyOne = true;
+
+  // this is boring .....
+
+
+//challenge 
+
+double doubleVar = 20d;
+double doubleVar2 = 80d;
+
+double outputVar = (doubleVar + doubleVar2) * 100;
+System.out.println(outputVar);
+double reminder = outputVar % 40;
+System.out.println(reminder);
+boolean outputReminder = (reminder == 0)? true : false;
+
+boolean finalOutput = outputReminder? false : true;
+
+if(finalOutput){
+  System.out.println("Got Some Reminder");
+}else{
+  System.out.println("got nothing");
+}
+
+
   }
 }
